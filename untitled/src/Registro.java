106806758be4440c0a4425c0b98dffcb9bf1eb67
AugustoTSS <139809087+AugustoTSS@users.noreply.github.com>
2024-11-08 20:58:42 -0300

    Update Registro.java

--- untitled/src/Registro.java
+++ untitled/src/Registro.java
@@ -1,9 +1,14 @@
 public class Registro {
+
+    // Atributo que armazena o código do registro.
     public int codigo;
+
+    // Atributo que armazena o valor de hash associado ao registro.
     public int hash;
 
+    // Construtor da classe Registro que inicializa os atributos 'codigo' e 'hash'.
     public Registro(int codigo, int hash) {
-        this.codigo = codigo;
-        this.hash = hash;
+        this.codigo = codigo;  // Atribui o valor do código ao atributo 'codigo'.
+        this.hash = hash;      // Atribui o valor do hash ao atributo 'hash'.
     }
 }
