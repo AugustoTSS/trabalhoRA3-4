6c94fdf45fce4ec1613f1a013fea8631e878b128
AugustoTSS <139809087+AugustoTSS@users.noreply.github.com>
2024-11-08 20:58:21 -0300

    Update Node.java

--- untitled/src/Node.java
+++ untitled/src/Node.java
@@ -1,13 +1,14 @@
 public class Node {
 
+    // Atributo que armazena o registro associado a este nó.
     public Registro registro;
+
+    // Atributo que aponta para o próximo nó na lista encadeada.
     public Node proximoNode;
 
+    // Construtor da classe Node, que inicializa o nó com um registro e define o próximo nó como null.
     public Node(Registro registro) {
-        this.registro = registro;
-        this.proximoNode = null;
+        this.registro = registro;  // Atribui o registro passado como parâmetro ao atributo 'registro'.
+        this.proximoNode = null;   // Inicializa o próximo nó como 'null', indicando que não há outro nó encadeado ainda.
     }
-
-
-
 }
